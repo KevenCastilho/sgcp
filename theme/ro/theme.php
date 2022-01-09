e4fd90dc2e4ee371f3481b54bfb8ddf6d299379e
Keven de Miranda Castilho <angelusinferni@users.noreply.github.com>
2022-01-09 09:43:18 -0300

    Initial commit

---
+++ theme/ro/theme.php
@@ -0,0 +1,15 @@
+<?php
+// Theme informations
+$THEME[name] = "Ragnarok Online";							// Theme Name
+$THEME[author] = "Venatu Ad Mortem";							// Theme Author
+$THEME[author_email] = "angelusinferni@live.com";						// Author E-mail
+$THEME[comment] = "Default Theme of cp";						// Theme Comment
+
+// Theme templates
+$THEME[background] = "bgcolor=\"#FFFFFF\"";						// Background
+$THEME[logo_img_url] = "images/logo.jpg";						// Logo's CP
+$THEME[logo_img_align] = "left";							// Logo's align
+$THEME[logo_background] = "bgcolor=\"#FFFFFF\"";					// Background Logo's CP
+$THEME[title_bar_img_url] = "img/title_bar.gif";
+$THEME[title_bar2_img_url] = "images/title_bar2.png";
+?>
